2dafb08a96045e9571ba14b087a4be89d790c2d6
Diego Brito <contato@diegobrito.com.br>
2011-11-20 13:40:09 -0200

    episodio_01

---
+++ episodio_01/app/controllers/paginas_controller.rb
@@ -0,0 +1,4 @@
+class PaginasController < ApplicationController
+  def index
+  end
+end
---
+++ episodio_01/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '2c5cb114bdcdf6a2e71ba66200bb4114c12cf3c29a9ef76219306a268d864d25ed523cf7de59831c28dc2c2f460ef547e8ca93940db0babede5d6a1e4ebd057d'
